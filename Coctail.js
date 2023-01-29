247c7f20bd460c2d0b2d4959270b39661e5e91d1
kmarenic <kresimir.marenic@biss.hr>
2023-01-29 13:19:57 +0100

    refactored and finished

---
+++ Coctail.js
@@ -0,0 +1,12 @@
+//import { Ingredient } from "./Ingredient";
+
+export class Coctail {
+        
+    constructor(id, name, picture, instructions, ingredients) {
+        this.id = id;
+        this.name = name;
+        this.picture = picture;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+    }
+}
--- script.js
+++ script.js
@@ -1,45 +1,34 @@
 'use strict'
 
+import { Coctail } from "./Coctail.js";
+
+const baseApiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/';
+
+
 const searchText = document.getElementById('coctail-input');
 const searchBtn = document.getElementById('search-btn');
 const coctailList = document.getElementById('found-coctails');    
 
-class Coctail {
-        
-    constructor(id, name, picture, instructions, ingredients) {
-        this.id = id;
-        this.name = name;
-        this.picture = picture;
-        this.instructions = instructions;
-        this.ingredients = ingredients;
-    }
-}
 
-const handleSearch = (e) => {
-    console.log('Clicked search');
+
+const handleSearch = (e) => {    
 
     const searchValue = searchText.value.trim();
 
     if (searchValue.length === 0) {
-        swal('Wrong input', 'Search input should have at least one letter.\nPlease drink a coctail and try again.', 'error');
-        //alert("Search input should have at least one letter.\nPlease drink a coctail and try again.");
-    } else {
-        const apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
+        swal('Wrong input', 'Search input should have at least one letter.\nPlease drink a coctail and try again.', 'error');        
+    } else {      
         const request = new XMLHttpRequest();
-        request.open('GET', apiUrl, true);
+        request.open('GET', `${baseApiUrl}search.php?s=${searchValue}`, true);
 
         request.onload = (e) => {
             console.log(request);
             if (request.status !== 200) {
                 swal('Something went wrong','Ooops. Your search hit the wrong road.\nPlease chill out, drink a coctail, take your time and try again later.', 'error');
-                //alert("Ooops. Your search hit the wrong road.\nPlease chill out, drink a coctail, take your time and try again later.");
             } else {
-                const response = JSON.parse(request.response);
-                console.log(response);
-                console.log(response.drinks);
+                const response = JSON.parse(request.response);                
                 if (response.drinks === null || response.drinks.length < 1) {
                     swal('Oh, no!', 'No coctails found.\nPlease chill out, think about some other search pattern and try again.', 'info');
-                    //alert('Oh, no!\nNo coctails found.\nPlease chill out, think and try again');
                 } else {
                     coctailList.innerHTML = '';
                     response.drinks.forEach(coctail => {
@@ -49,10 +38,10 @@ const handleSearch = (e) => {
                 }                              
             } 
         }
-
         request.send();
-    }    
+    }       
 }
+
 
 
 const handleInputKey = (e) => {
@@ -66,35 +55,25 @@ searchText.addEventListener('keyup', handleInputKey);
 
 
 const handleDetails = (e) => {
-    const apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${e.target.id}`;
-        const request = new XMLHttpRequest();
-        request.open('GET', apiUrl, true);
+    const request = new XMLHttpRequest();
+    request.open('GET', `${baseApiUrl}lookup.php?i=${e.target.id}`, true);
 
-        request.onload = (e) => {
-            console.log(request);
+    request.onload = (e) => {
+        console.log(request);
             const response = JSON.parse(request.response).drinks[0];
-            console.log(response);
-            
-            const ingredients = [];
-
-            for (const attr in response) {
-                if(attr.includes('strIngredient') && response[attr] !== null) {
-                    ingredients.push(response[attr]);
-                }                
-            }
-
-            const coctail = new Coctail(response.idDrink, response.strDrink, response.strDrinkThumb, response.strInstructions, ingredients);
-            console.log(coctail);
+            const coctail = createDetailedCoctailObject(response);
             populateCoctailDetails(coctail);
+    }
+    request.send();
+}
 
-            
-
-            
-            
-
-        }
-
-        request.send();
+const handleActive = (e) => {
+    const activeElement = document.getElementById(e.target.id);
+    const parent = activeElement.parentElement;
+    parent.childNodes.forEach(child => {
+        child.classList.remove('active');
+    });    
+    activeElement.classList.add('active');
 }
 
 
@@ -104,13 +83,23 @@ function populateSearchResult(foundCoctail) {
     newCoctail.innerText = `${foundCoctail.name}`;
     newCoctail.setAttribute('id', `${foundCoctail.id}`)
     newCoctail.addEventListener('click', handleDetails);
+    newCoctail.addEventListener('click', handleActive);
 
     coctailList.append(newCoctail);
 }
 
-function  populateCoctailDetails(coctail) {
+function createDetailedCoctailObject(response) {
+    const ingredients = [];
 
-    
+    for (const attr in response) {
+        if(attr.includes('strIngredient') && response[attr] !== null) {
+            ingredients.push(`${response[attr]} - ${response[attr.replace('Ingredient','Measure')]}`);
+        }                
+    }
+    return new Coctail(response.idDrink, response.strDrink, response.strDrinkThumb, response.strInstructions, ingredients);
+}
+
+function  populateCoctailDetails(coctail) {    
 
     const detailsContainer = document.getElementsByClassName('details')[0];
     detailsContainer.innerHTML = '';
@@ -121,11 +110,7 @@ function  populateCoctailDetails(coctail) {
 
     const detailsPicture = document.createElement('img');
     detailsPicture.src = coctail.picture;
-    detailsContainer.append(detailsPicture);
-
-    const detailsInstructions = document.createElement('div');
-    detailsInstructions.innerHTML = `<h4>Instructions:</h4> ${coctail.instructions}`;
-    detailsContainer.append(detailsInstructions);
+    detailsContainer.append(detailsPicture);   
 
     const detailsIngedientsTitle = document.createElement('h4');
     detailsIngedientsTitle.innerText = 'Ingredients';
@@ -140,14 +125,9 @@ function  populateCoctailDetails(coctail) {
     });
     detailsContainer.append(detailsIngredients);
 
-    
-    
-
-
-    
-    
-
-
-};
+    const detailsInstructions = document.createElement('div');
+    detailsInstructions.innerHTML = `<h4>Instructions:</h4> ${coctail.instructions}`;
+    detailsContainer.append(detailsInstructions);
+}
 
 
